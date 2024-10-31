8b4e93919bda7adf22ca00f5d8698ba04e52a7fc
ayman saleh <ayman.saleh000@gmail.com>
2024-10-31 13:59:47 +0300

    intial

---
+++ src/app/dashboard/dash-items/dash-items.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './dash-items.component.html',
+  styleUrl: './dash-items.component.css'
+})
+export class DashItemsComponent {
+  // @Input({required: true})  image!: {scr: string; alt: string};
+  // @Input({required: true}) title!: string;
+
+  image = input.required<{scr: string; alt: string}>();
+ title = input.required<string>();
+}
