c775b8188b3da200a5220fe1e6c6f4d3959ba96f
ayman saleh <ayman.saleh000@gmail.com>
2024-10-31 16:04:26 +0300

    modyfied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
-import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashItemsComponent } from './dashboard/dash-items/dash-items.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent,TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TicketsComponent, DashItemsComponent, TrafficComponent],
 })
 export class AppComponent {
 }
--- src/app/dashboard/dash-items/dash-items.component.ts
+++ src/app/dashboard/dash-items/dash-items.component.ts
@@ -11,6 +11,6 @@ export class DashItemsComponent {
   // @Input({required: true})  image!: {scr: string; alt: string};
   // @Input({required: true}) title!: string;
 
-  image = input.required<{scr: string; alt: string}>();
+  image = input.required<{src: string; alt: string}>();
  title = input.required<string>();
 }
